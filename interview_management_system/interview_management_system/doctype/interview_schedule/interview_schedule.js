55b079102f10fe1ecb84091efda586c1e20c4fe1
Akshata078 <akshatakhadse123@gmail.com>
2024-12-12 12:22:25 +0530

    new-feature

--- interview_management_system/interview_management_system/doctype/interview_schedule/interview_schedule.js
+++ interview_management_system/interview_management_system/doctype/interview_schedule/interview_schedule.js
@@ -1,8 +1,31 @@
 // Copyright (c) 2024, Akshata and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Interview Schedule", {
-// 	refresh(frm) {
+frappe.ui.form.on("Interview Schedule", {
+    refresh(frm) {
+       if(frm.doc.status === "In Progress"){
+        frm.add_custom_button("Hire",()=>{
+            frm.set_value("status","Hired");
+            frm.save();
+        });
+        frm.add_custom_button("Reject",()=>{
+            frm.set_value("status","Rejected");
+            frm.save();
+        })
+       }
 
-// 	},
-// });
+       if(frm.doc.status === "Hired"){
+        frm.add_custom_button("Reject",()=>{
+            frm.set_value("status","Rejected");
+            frm.save();
+        });
+       }
+    },
+    onload(frm){
+        frappe.show_alert(`You are viewing ${frm.doc.candidate_name}'s document!`)
+    },
+    on_trash(frm){
+        frappe.show_alert("deleted successfully")
+    }
+});
+
--- interview_management_system/interview_management_system/doctype/interview_schedule/interview_schedule.py
+++ interview_management_system/interview_management_system/doctype/interview_schedule/interview_schedule.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2024, Akshata and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from frappe.utils import today, getdate
 
 class InterviewSchedule(Document):
-	pass
+    def validate(self):
+        current_date = getdate(today())
+        selected_date = getdate(self.interview_date) 
+        
+        if selected_date < current_date:
+            frappe.throw("You can't select a previous date for the interview.")
